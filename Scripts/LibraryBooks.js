4a853125db4051188eec447310994770d94d952c
asterwolf <diego.diaz1994@email.bakersfieldcollege.edu>
2016-11-20 12:52:14 -0800

    updating

---
+++ Scripts/LibraryBooks.js
@@ -0,0 +1,50 @@
+var arrayOfBooks = [
+	var book1 = { 
+		title: "Lord of the Rings", 
+		author: "Tolkien, J. R. R.", 
+		genre: "adventure", 
+		number: "PR6039.032 L63 1965", 
+		floorLevel: 1, 
+		availability: true 
+	}
+	var book2 = {
+		title: "Harry Potter and the sorcerer's stone",
+		author: "J. K. Rowling",,
+		genre: "fantasy",
+		number: "PR6068.093 h377 v.1",
+		floorLevel: 1,
+		availability: true
+	}
+	var book3 = {
+		title: "Harry Potter and the Chamber of Secrets",
+		author: "J. K. Rowling",
+		genre: "fantasy"
+		number: "PR6068.093 H377 v.2",
+		floorLevel: 1,
+		availability: false
+	}
+	var book4 = {
+		title: "Harry Potter and the Deathly Hallows",
+		author: "J. K. Rowling",
+		genre: "fantasy",
+		number: "PR6068.093 H377 v.7",
+		floorLevel: 1,
+		availability: false
+	}
+	var book5 = {
+		title: "Star wars Episode I: the phantom menace",
+		author: "Terry Brooks",
+		genre: "sci-fi",
+		number: "PS3552.R6596 S73 1999",
+		floorLevel: 4,
+		availability: true
+	}
+	var book6 = {
+		title: "Breaking dawn",
+		author: "Stephenie Meyer",
+		genre: "adventure"
+		number: "PS3613.E979 B743 2009"
+		floorLevel: 1
+		availability: true
+	}	
+];
